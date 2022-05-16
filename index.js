58e61d3b7bb01abdc59aacf41217ddcdd321888e
ankurrimal3 <ankurrimal3@gmail.com>
2022-05-16 12:30:40 +0545

    routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose")
 const dotenv=require("dotenv")
+const userRoute=require('./routes/user')
 
 dotenv.config();
 
@@ -9,7 +10,9 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log('db connection successful'))
 .catch((err)=>console.log(err))
 
+app.use(express.json())
+app.use("/api/user",userRoute)
 
-app.listen(5000,(req,res)=>{
+app.listen(process.env.PORT,(req,res)=>{
     console.log('backend running successfully')
 })
