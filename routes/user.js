3c4768da9768926f4bfeb2c1ad6181ea8bab06a1
ankurrimal3 <ankurrimal3@gmail.com>
2022-05-16 15:53:57 +0545

    get update delete user 1:14

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,58 @@
 const router=require("express").Router();
-const verifyToken=require("./verifyToken")
+const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin}=require("./verifyToken")
 const User=require('../models/User')
 
-router.put("/:id",verifyToken.verifyTokenAndAuthorization, async(req,res)=>{
-    if(req.body.password) {
-        req.body.password=CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
     }
-    try{
-        const updatedUser =await User.findByIdAndUpdate(req.params.id, {
-            $set:req.body
-        },{new:true})
-        res.status(200).json(updatedUser)
+  
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
     }
-    catch(err){
+  });
+
+router.delete("/:id", verifyTokenAndAuthorization,async(req,res)=>{
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json('deleted successfully')
+    }
+    catch(err) {
         res.status(500).json(err)
     }
-   
+})
+
+router.get("/find/:id", verifyTokenAndAdmin,async(req,res)=>{
+    try {
+        const user =await User.findById(req.params.id)
+        const{password, ...others}=user._doc
+        res.status(200).json(others)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+router.get("/", verifyTokenAndAdmin,async(req,res)=>{
+    try {
+        const users =await User.find()
+        // const{password, ...others}=user._doc
+        res.status(200).json(users)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
 })
 
 
