39deb467df7fa15a02307c61c73a0fcb26d9c60b
ankurrimal3 <ankurrimal3@gmail.com>
2022-05-16 14:03:29 +0545

    passoword hashing, and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router=require("express").Router();
 const User=require('../models/User')
+const CryptoJS=require('crypto-js')
 
 //register
 
@@ -7,7 +8,7 @@ router.post("/register", async(req,res)=>{
     const newUser=new User({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password
+        password:CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
     //will save to db
     try {
@@ -16,6 +17,25 @@ router.post("/register", async(req,res)=>{
     }
     catch(err) {
         res.status(500).json(err)
+    }
+})
+
+//login
+router.post("/login", async(req,res)=>{
+    try {
+        const user =await User.findOne({username:req.body.username});
+        !user && res.status(401).json("Wrong Username")
+
+        const hashedPassword=CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+        const password=hashedPassword.toString(CryptoJS.enc.Utf8)
+        
+        password !== req.body.password && res.status(401).json('Wrong password')
+        res.status(200).json(user)
+
+
+    }
+    catch(err){
+        res.status(501).json(err)
     }
 })
 
