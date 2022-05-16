2e2cdd7e641582a3305ca332085ec21b14bbc60b
ankurrimal3 <ankurrimal3@gmail.com>
2022-05-16 16:48:37 +0545

    crud operation in all modules 1:46

--- routes/user.js
+++ routes/user.js
@@ -45,8 +45,9 @@ router.get("/find/:id", verifyTokenAndAdmin,async(req,res)=>{
     }
 })
 router.get("/", verifyTokenAndAdmin,async(req,res)=>{
+    const query=req.query.new
     try {
-        const users =await User.find()
+        const users =query ? await User.find().sort({_id:-1}).limit(2) : await User.find()
         // const{password, ...others}=user._doc
         res.status(200).json(users)
     }
@@ -54,6 +55,31 @@ router.get("/", verifyTokenAndAdmin,async(req,res)=>{
         res.status(500).json(err)
     }
 })
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  
+    try {
+      const data = await User.aggregate([
+        { $match: { createdAt: { $gte: lastYear } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1 },
+          },
+        },
+      ]);
+      res.status(200).json(data)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 module.exports=router;
