2fa1137e569579e038bf9fdc77011382771f9b8a
ankurrimal3 <ankurrimal3@gmail.com>
2022-05-16 14:19:28 +0545

    access token implemented successfully

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router=require("express").Router();
 const User=require('../models/User')
 const CryptoJS=require('crypto-js')
+const jwt=require('jsonwebtoken')
 
 //register
 
@@ -27,15 +28,24 @@ router.post("/login", async(req,res)=>{
         !user && res.status(401).json("Wrong Username")
 
         const hashedPassword=CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
-        const password=hashedPassword.toString(CryptoJS.enc.Utf8)
+        const OriginalPassword=hashedPassword.toString(CryptoJS.enc.Utf8)
         
-        password !== req.body.password && res.status(401).json('Wrong password')
-        res.status(200).json(user)
+        OriginalPassword !== req.body.password && res.status(401).json('Wrong password')
+
+        const accessToken=jwt.sign({
+            id:user._id,
+            isAdmin:user.isAdmin
+        }, process.env.JWT_SEC,
+        {expiresIn:"3d"})
+        
+        const{password, ...others}=user._doc;
+
+        res.status(200).json({...others, accessToken})
 
 
     }
     catch(err){
-        res.status(501).json(err)
+        res.status(500).json(err)
     }
 })
 
