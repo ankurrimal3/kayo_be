4f270a059baad4c394bfca37fc4defba6510eea2
ankurrimal3 <ankurrimal3@gmail.com>
2022-05-18 11:11:34 +0545

    stripe in be, integration started

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const authRoute=require('./routes/auth')
 const productRoute=require('./routes/product')
 const cartRoute=require('./routes/cart')
 const orderRoute=require('./routes/order')
+const stripeRoute=require('./routes/stripe')
+
+const cors=require("cors")
 
 
 dotenv.config();
@@ -15,12 +18,15 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log('db connection successful'))
 .catch((err)=>console.log(err))
 
+app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/user",userRoute)
 app.use("/api/product", productRoute)
 app.use("/api/cart", cartRoute)
 app.use("/api/order", orderRoute)
+app.use("/api/checkout", stripeRoute)
+
 
 
 app.listen(process.env.PORT,(req,res)=>{
